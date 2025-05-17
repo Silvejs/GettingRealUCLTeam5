0a2d994f6b9c24b41cb5cffe146592ced253a78a
Nickolai-Nygaard <nygaardnickolai@gmail.com>
2025-05-18 00:36:32 +0200

    Guest#Guest closes

---
+++ DagmarsBodegaBooking/DagmarsBodegaBooking/Models/Guest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DagmarsBodegaBooking.Models
+{
+    pubilc class Guest
+    {
+        public string Name { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Mail { get; set; }
+        public int GuestId { get; set; }
+    }
+}
