121cb57b7d35ec181618bfcef7992cbcc14a2eae
Silviu Partac <nikkyspeedy@yahoo.com>
2025-05-19 18:28:52 +0200

    Id generator for guest klassen - lavet i metoden GuestIdGen

--- DagmarsBodegaBooking/DagmarsBodegaBooking/Models/Guest.cs
+++ DagmarsBodegaBooking/DagmarsBodegaBooking/Models/Guest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,5 +13,53 @@ namespace DagmarsBodegaBooking.Models
         public int PhoneNumber { get; set; }
         public string Mail { get; set; }
         public int GuestId { get; set; }
+
+
+        public Guest(int guestId, string name, int phoneNumber, string mail)
+        {
+            GuestId = guestId;
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Mail = mail;
+        }
+
+        public int GuestIdGen()
+        {
+            Random idGen = new Random();            
+            List<int> usedGuestIds =new List<int>();
+
+            //man kan fx bruge GUID men det er ret langt og meget kompleks => 4 bord x 365 dage = 1460 nr brugte hvert år ved en all-bord booking hver dag
+            // var guestId = Guid.NewGuid(); 
+
+            int rangeStart = 1000;
+            int rangeStop = 9999;
+
+
+            if (usedGuestIds.Count >= rangeStop - rangeStart +1)
+            {
+                //hvis alle nr mellem 1000 og 9999 er brugte, så vil den ny min og max blive afregnet så den nye min bliver 10 000  og den nye max max blive 99 999
+                rangeStart = rangeStop + 1;
+                rangeStop = rangeStart * 10 - 1;
+            }
+
+
+            do
+            {
+                GuestId = idGen.Next(rangeStart, rangeStop+1);
+            }
+            while (usedGuestIds.Contains(GuestId));
+
+
+
+            usedGuestIds.Add(GuestId);
+            return GuestId;
+        }
+
+
+
+        
+
+
+
     }
 }
