5a687fdf84c30666fbb28675dd148f676720a977
Rebecca P <rebecca_pedersen@msn.com>
2025-05-20 09:53:13 +0200

    Ændret TimeSpan og tilføjet nameTable

--- DagmarsBodegaBooking/DagmarsBodegaBooking/Models/Booking.cs
+++ DagmarsBodegaBooking/DagmarsBodegaBooking/Models/Booking.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace DagmarsBodegaBooking.Models
 {
@@ -10,12 +11,13 @@ namespace DagmarsBodegaBooking.Models
     {
         public int NumGuests { get; set; }
         public DateTime Date { get; set; }
-        public TimeSpan Time { get; set; }
+        public DateTime Time { get; set; }
         public string Comment { get; set; }
         public int Id { get; set; }
         public int GuestId { get; set; }
+        public string NameTable { get; set; }
 
-        public Booking(int numGuests, DateTime date, TimeSpan time, string comment, int id, int guestId)
+    public Booking(int numGuests, DateTime date, DateTime time, string comment, int id, int guestId, string nameTable)
         {
             NumGuests = numGuests;
             Date = date;
@@ -23,11 +25,12 @@ namespace DagmarsBodegaBooking.Models
             Comment = comment;
             Id = id;
             GuestId = guestId;
+            NameTable = nameTable;
         }
 
         public override string ToString()
         {
-            return $"{NumGuests};{Date.ToShortDateString()};{Time};{Comment};{Id};{GuestId}";
+            return $"{NumGuests};{Date.ToShortDateString()};{Time};{Comment};{Id};{GuestId};{NameTable}";
         }
 
         public static Booking FromString(string data)
@@ -40,12 +43,13 @@ namespace DagmarsBodegaBooking.Models
 
             int numGuests = int.Parse(parts[0]);
             DateTime date = DateTime.Parse(parts[1]);
-            TimeSpan time = TimeSpan.Parse(parts[2]);
+            DateTime time = DateTime.Parse(parts[2]);
             string comment = parts[3];
             int id = int.Parse(parts[4]);
             int guestId = int.Parse(parts[5]);
+            string nameTable = parts[6];
 
-            return new Booking(numGuests, date, time, comment, id, guestId);
+            return new Booking(numGuests, date, time, comment, id, guestId, nameTable);
         }
     }
 }
