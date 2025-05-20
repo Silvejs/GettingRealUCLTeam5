bf14910f40e02c0669863ad1aba980034156f3fd
Rebecca P <rebecca_pedersen@msn.com>
2025-05-20 10:20:57 +0200

    ændret til bookingid

--- DagmarsBodegaBooking/DagmarsBodegaBooking/Models/Booking.cs
+++ DagmarsBodegaBooking/DagmarsBodegaBooking/Models/Booking.cs
@@ -13,24 +13,24 @@ namespace DagmarsBodegaBooking.Models
         public DateTime Date { get; set; }
         public DateTime Time { get; set; }
         public string Comment { get; set; }
-        public int Id { get; set; }
+        public int BookingId { get; set; }
         public int GuestId { get; set; }
         public string NameTable { get; set; }
 
-    public Booking(int numGuests, DateTime date, DateTime time, string comment, int id, int guestId, string nameTable)
+    public Booking(int numGuests, DateTime date, DateTime time, string comment, int bookingId, int guestId, string nameTable)
         {
             NumGuests = numGuests;
             Date = date;
             Time = time;
             Comment = comment;
-            Id = id;
+            BookingId = bookingId;
             GuestId = guestId;
             NameTable = nameTable;
         }
 
         public override string ToString()
         {
-            return $"{NumGuests};{Date.ToShortDateString()};{Time};{Comment};{Id};{GuestId};{NameTable}";
+            return $"{NumGuests};{Date.ToShortDateString()};{Time};{Comment};{BookingId};{GuestId};{NameTable}";
         }
 
         public static Booking FromString(string data)
@@ -45,11 +45,11 @@ namespace DagmarsBodegaBooking.Models
             DateTime date = DateTime.Parse(parts[1]);
             DateTime time = DateTime.Parse(parts[2]);
             string comment = parts[3];
-            int id = int.Parse(parts[4]);
+            int bookingId = int.Parse(parts[4]);
             int guestId = int.Parse(parts[5]);
             string nameTable = parts[6];
 
-            return new Booking(numGuests, date, time, comment, id, guestId, nameTable);
+            return new Booking(numGuests, date, time, comment, bookingId, guestId, nameTable);
         }
     }
 }
