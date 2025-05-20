b66672bbec0336b9bb8faf51c390b48f3f3c389f
Silviu Partac <nikkyspeedy@yahoo.com>
2025-05-20 10:44:28 +0200

    Gues.EtAll - opdateret

--- DagmarsBodegaBooking/DagmarsBodegaBooking/Models/Guest.cs
+++ DagmarsBodegaBooking/DagmarsBodegaBooking/Models/Guest.cs
@@ -24,38 +24,7 @@ namespace DagmarsBodegaBooking.Models
             Mail = mail;
         }
 
-        public int GuestIdGen()                 //Guest Id Generator Metode
-        {
-            Random idGen = new Random();            
-            List<int> usedGuestIds =new List<int>();
-
-            //man kan fx bruge GUID men det er ret langt og meget kompleks => 4 bord x 365 dage = 1460 nr brugte hvert år ved en all-bord booking hver dag
-            // var guestId = Guid.NewGuid(); 
-
-            int rangeStart = 1000;
-            int rangeStop = 9999;
-
-
-            if (usedGuestIds.Count >= rangeStop - rangeStart + 1)
-            {
-                //hvis alle nr mellem 1000 og 9999 er brugte, så vil den ny min og max blive afregnet så den nye min bliver 10 000  og den nye max max blive 99 999
-                rangeStart = rangeStop + 1;
-                rangeStop = rangeStart * 10 - 1;
-            }
-
-                                        //  do - while loop 
-            do
-            {
-                GuestId = idGen.Next(rangeStart, rangeStop+1);
-            }
-            while (usedGuestIds.Contains(GuestId));
-
-
-
-            usedGuestIds.Add(GuestId);
-            return GuestId;
-        }
-
+      
       
         
         public override string ToString()           //method override ToString() - Persistens
